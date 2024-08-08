afd8266b33089bab9d81e0dfba15258fe63cc41a
Ixecd <2192629378@qq.com>
2024-08-09 00:23:05 +0800

    dp

--- DP.cc
+++ DP.cc
@@ -17,6 +17,48 @@ void backtricking(vector<int> &choice, int cur, int n, int &res) {
     }
 }
 
+int climb(int n, vector<int> const& cost) {
+    if (n == 1 || n == 2) return cost[0];
+    vector<int> dp(n + 1, 0);
+    for (int i = 3; i <= n; ++i) 
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    return dp[n];
+}
+
+int climb2(int n, vector<int> const& cost) {
+    if (n == 1 || n == 2) return cost[n];
+    vector<int> dp(n + 1, 0);
+    for (int i = 3; i <= n; ++i) 
+        dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+    return dp[n];
+}
+
+int climb3(int n, vector<int> const& cost) {
+    if (n == 1 || n == 2) return cost[n];
+    int a = cost[1], b = cost[2];
+    for (int i = 3; i <= n; ++i) {
+        int tmp = b;
+        b = min(a, b) + cost[i];
+        a = tmp;
+    }
+    return b;
+}
+
+void Solution(vector<vector<int>> const &grid) {
+    int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    dp[1][1] = grid[0][0];
+    for (int i = 2; i <= n; ++i) 
+        dp[i][1] += dp[i - 1][1] + grid[i - 1][0];
+    for (int j = 2; j <= m; ++j) 
+        dp[1][j] += dp[1][j - 1] + grid[0][j - 1];
+
+    for (int i = 2; i <= n; ++i) 
+        for (int j = 2; j <= m; ++j) 
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+    cout << dp[n][m] << endl;
+}
+
 int main() {
     int cur = 0;
 
