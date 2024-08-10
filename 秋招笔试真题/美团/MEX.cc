2eef9df24db14ad5a9d7e23ccf6095b3e0277922
Ixecd <2192629378@qq.com>
2024-08-10 23:06:28 +0800

    秋招

--- 秋招笔试真题/美团/MEX.cc
+++ 秋招笔试真题/美团/MEX.cc
@@ -29,6 +29,18 @@ int firstMissingPosAndZero(vector<int> &nums) {
     return n;
 }
 
+// 缺失的第一个正数
+int firstMissingPositivity(vector<int> &nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) 
+        while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            swap(nums[i], nums[nums[i] - 1]);
+    for (int i = 0; i < n; ++i) {
+        if (nums[i] != i + 1) return i + 1;
+    }
+    return n + 1;
+}
+
 int main() {
     int T;
     cin >> T;
