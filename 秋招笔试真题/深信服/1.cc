063d5f6993eae559672c136216ee17eba76be6a8
Ixecd <2192629378@qq.com>
2024-08-23 21:37:38 +0800

    leetcode

---
+++ 秋招笔试真题/深信服/1.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+// 石头 RGB三种颜色的石头 88%
+int main() {
+
+    
+
+    return 0;
+}
---
+++ 秋招笔试真题/深信服/4.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+// 最大金币
+int main() {
+
+    
+
+    return 0;
+}
