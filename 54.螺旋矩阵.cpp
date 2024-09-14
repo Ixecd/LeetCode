16288ea9780180da858e850a860a5bb551361e13
Ixecd <2192629378@qq.com>
2024-09-14 18:20:34 +0800

    秋招

--- 54.螺旋矩阵.cpp
+++ 54.螺旋矩阵.cpp
@@ -10,36 +10,34 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>> &matrix) {
-        int m = matrix.size(), n = m ? matrix[0].size() : 0, u = 0, d = m - 1,
-            l = 0, r = n - 1, p = 0;
-        vector<int> order(m * n);
-        while (u <= d && l <= r) {
-            for (int col = l; col <= r; col++) {
-                order[p++] = matrix[u][col];
-            }
-            if (++u > d) {
-                break;
-            }
-            for (int row = u; row <= d; row++) {
-                order[p++] = matrix[row][r];
-            }
-            if (--r < l) {
-                break;
-            }
-            for (int col = r; col >= l; col--) {
-                order[p++] = matrix[d][col];
-            }
-            if (--d < u) {
-                break;
-            }
-            for (int row = d; row >= u; row--) {
-                order[p++] = matrix[row][l];
-            }
-            if (l++ > r) {
-                break;
-            }
+        // 从矩阵到数组四个变量
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return {};
         }
-        return order;
+        vector<int> ans;
+        int m = matrix.size(), n = matrix[0].size();
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; ++i) {
+                ans.push_back(matrix[top][i]);
+            }
+            for (int i = top + 1; i <= bottom; ++i) {
+                ans.push_back(matrix[i][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int i = right - 1; i >= left; --i) {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                for (int i = bottom - 1; i > top; --i) {
+                    ans.push_back(matrix[i][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return ans;
     }
 };
 
